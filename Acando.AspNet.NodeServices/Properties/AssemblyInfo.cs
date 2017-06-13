71c51a1ceb6794db4948d6c84891b7beb0cc976b
Barlind <tobias@barlind.se>
2017-06-13 16:26:00 +0200

    Added .sln

--- Acando.AspNet.NodeServices/Properties/AssemblyInfo.cs
+++ Acando.AspNet.NodeServices/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Acando.AspNet.NodeServics")]
+[assembly: AssemblyTitle("Acando.AspNet.NodeServices")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Acando.AspNet.NodeServics")]
+[assembly: AssemblyProduct("Acando.AspNet.NodeServices")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
